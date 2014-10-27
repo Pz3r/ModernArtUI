a6da078a65447309c1db7cedf042103ff2aeeec6
Samuel Pozaicer <sam.pz3r@gmail.com>
2014-10-26 20:09:25 -0600

    Info Dialog
    Implemented “Learn More” Dialog.

--- src/com/example/modernartui/MainActivity.java
+++ src/com/example/modernartui/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.modernartui;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,10 +30,40 @@ public class MainActivity extends ActionBarActivity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		switch(item.getItemId()){
+		case R.id.action_settings:
+			new VisitMomaDialogFragment().show(getSupportFragmentManager(), "visitMoma");
 			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
+	}
+	
+	private class VisitMomaDialogFragment extends DialogFragment{
+		
+		@Override
+		@NonNull
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setMessage(R.string.alert_moma_message)
+			.setPositiveButton(R.string.alert_moma_positive, new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Go to activity with a web view showing moma's website
+					
+				}
+			})
+			.setNegativeButton(R.string.alert_moma_negative, new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dismiss();
+				}
+			});
+			
+			return builder.create();
+		}
+		
 	}
 }

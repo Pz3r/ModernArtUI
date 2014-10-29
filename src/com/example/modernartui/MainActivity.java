d881795e4cf3c5e1ab43d55b00b38c6cd3171154
Samuel Pozaicer <sam.pz3r@gmail.com>
2014-10-28 18:55:33 -0600

    First release finished

--- src/com/example/modernartui/MainActivity.java
+++ src/com/example/modernartui/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.modernartui;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -15,96 +13,58 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+		OnSeekBarChangeListener {
 
-	// Random number generator
-	private Random ran = new Random();
+	private SeekBar seekbar;
+	private LinearLayout rectContainer;
+	private LinearLayout rectSubContainer;
+	private View blackRect;
+	private View blueRect;
+	private View brownRect1;
+	private View brownRect2;
+	private View brownRect3;
+	private View dgreyRect1;
+	private View dgreyRect2;
+	private View greyRect1;
+	private View greyRect2;
+	private View greyRect3;
+	private View redRect;
 
-	// Define maximum weight a view can have
-	private static final int MAX_WEIGHT = 4;
+	private int lastProgress;
 
-	// Define number of rows per column
-	private static final int MAX_ROWS = 6;
+	private static final String MOMA_URL = "http://www.moma.org/collection/artist.php?artist_id=4293";
 
-	// Available colors array
-	private static final int[] AVAILABLE_COLORS = { R.color.blue,
-			R.color.purple, R.color.green, R.color.orange, R.color.red,
-			R.color.grey };
+	// Value to be added or subtracted from the view whenever the SeekBar is
+	// dragged
+	private static final float WEIGHT_OFFSET = 0.1f;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//LinearLayout layoutContainer = (LinearLayout) findViewById(R.id.layout_container);
-		//paintLayout(layoutContainer);
+		// Get references to the Views and Layout declared in the layout file
+		rectContainer = (LinearLayout) findViewById(R.id.rectangles_container);
+		rectSubContainer = (LinearLayout) findViewById(R.id.rectangle_subcontainer_2);
+		blackRect = findViewById(R.id.black_rectangle);
+		blueRect = findViewById(R.id.blue_rectangle);
+		brownRect1 = findViewById(R.id.brown_rectangle_1);
+		brownRect2 = findViewById(R.id.brown_rectangle_2);
+		brownRect3 = findViewById(R.id.brown_rectangle_3);
+		dgreyRect1 = findViewById(R.id.dark_grey_rectangle_1);
+		dgreyRect2 = findViewById(R.id.dark_grey_rectangle_2);
+		greyRect1 = findViewById(R.id.grey_rectangle_1);
+		greyRect2 = findViewById(R.id.grey_rectangle_2);
+		greyRect3 = findViewById(R.id.grey_rectangle_3);
+		redRect = findViewById(R.id.red_rectangle);
 
-	}
+		seekbar = (SeekBar) findViewById(R.id.seekbar);
+		seekbar.setOnSeekBarChangeListener(this);
 
-	private void paintLayout(LinearLayout container) {
-
-		// Get the number of columns, each columns is represented by a
-		// LinearLayout child from layout_container
-		int columnCount = container.getChildCount();
-
-		// Populate each LinearLayout column with Views
-		LinearLayout column;
-
-		// ArrayList used to keep track of which colors have been already used
-		// in each row
-		ArrayList<Integer> colors;
-
-		// View to be added
-		View tempView;
-
-		for (int i = 0; i < columnCount; i++) {
-			column = (LinearLayout) container.getChildAt(i);
-			colors = getArrayListFromArray(AVAILABLE_COLORS);
-			// Generate MAX_ROWS randomized views and add them to the current
-			// column
-			for (int j = 0; j < MAX_ROWS; j++) {
-				tempView = generateRandomizedView();
-				colorView(colors, tempView);
-				column.addView(tempView);
-			}
-		}
-	}
-
-	// Method for generating a View with a randomized weight.
-	private View generateRandomizedView() {
-
-		int weight = ran.nextInt(MAX_WEIGHT) + 1;
-
-		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, 0,
-				weight);
-
-		View view = new View(this);
-		view.setLayoutParams(params);
-
-		return view;
-	}
-
-	// Select a Random color from an ArrayList containing the id's from the
-	// available color resources, a color can only be used once per row
-	private void colorView(ArrayList<Integer> colors, View view) {
-		if (colors != null && view != null) {
-			int position = ran.nextInt(colors.size());
-			int color = (Integer) (colors.get(position));
-
-			view.setBackgroundResource(color);
-			// Remove used color from the list so it cannot be used again.
-			colors.remove(position);
-		}
-	}
-
-	private ArrayList<Integer> getArrayListFromArray(int[] array) {
-		ArrayList<Integer> temp = new ArrayList<Integer>();
-		for (int i = 0; i < array.length; i++) {
-			temp.add(array[i]);
-		}
-		return temp;
 	}
 
 	@Override
@@ -142,8 +102,13 @@ public class MainActivity extends ActionBarActivity {
 								@Override
 								public void onClick(DialogInterface dialog,
 										int which) {
-									// TODO Go to activity with a web view
-									// showing moma's website
+									Intent intent = new Intent(
+											Intent.ACTION_VIEW, Uri
+													.parse(MOMA_URL));
+									if (intent
+											.resolveActivity(getPackageManager()) != null) {
+										startActivity(intent);
+									}
 
 								}
 							})
@@ -160,5 +125,57 @@ public class MainActivity extends ActionBarActivity {
 			return builder.create();
 		}
 
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+
+		// Determine difference between last and current progress (user can skip
+		// steps touching the SeekBar)
+		int diff = progress - lastProgress;
+
+		// This value determines direction and by how much the weight will be
+		// affected
+		float offset = WEIGHT_OFFSET * diff;
+
+		lastProgress = progress;
+
+		// Adjust weight and color in the different views to give the effect of
+		// movement
+		setViewWeightColor(blackRect, offset, progress);
+		setViewWeightColor(blueRect, -offset, progress);
+		setViewWeightColor(redRect, 0, progress);
+		setViewWeightColor(brownRect1, offset, progress);
+		setViewWeightColor(greyRect1, offset, progress);
+		setViewWeightColor(dgreyRect1, -offset, progress);
+		setViewWeightColor(dgreyRect2, offset, progress);
+		setViewWeightColor(brownRect2, 0, progress);
+		setViewWeightColor(greyRect2, -offset, progress);
+		setViewWeightColor(greyRect3, offset, progress);
+		setViewWeightColor(brownRect3, -offset, progress);
+
+		// Needed for views to redraw themselves given their new weight values
+		rectContainer.requestLayout();
+		rectSubContainer.requestLayout();
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void setViewWeightColor(View view, float weightOffSet, int progress) {
+		// Change the weight of each the view by the value in weightOffSet
+		((LinearLayout.LayoutParams) view.getLayoutParams()).weight += weightOffSet;
+		// Level used with level lists in order to change color
+		view.getBackground().mutate().setLevel(progress);
 	}
 }
